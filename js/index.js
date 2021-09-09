c79e941dfc960e5af74f87ac7207d29f05168919
TumRittidej <w.rittidej@gmail.com>
2021-09-09 20:02:09 +0700

    test55

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const showMenu = document.getElementById('showmenu');
+
+function openNavMenu() {
+    showMenu.classList.add('togglemenu')
+    console.log("1")
+}
+
+function closeNavMenu() {
+    showMenu.classList.remove('togglemenu')
+}
