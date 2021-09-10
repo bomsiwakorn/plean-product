8c110bf1b90d7eb6868dd369e08550ee1474c015
Bomsiwakorn <bomsiwakorns@gmail.com>
2021-09-11 00:34:32 +0700

    Modal/Gallery

--- js/index.js
+++ js/index.js
@@ -3,16 +3,39 @@
 const showMenu = document.getElementById('showmenu');
 
 function openNavMenu() {
-    showMenu.classList.add('togglemenu')
+  showMenu.classList.add('togglemenu');
 }
 
 function closeNavMenu() {
-    showMenu.classList.remove('togglemenu')
+  showMenu.classList.remove('togglemenu');
 }
 
 // active select product
 
+function showGalleryModal(index) {
+  const galleryModal = document.querySelector('.gallery-plean-modal');
+  const bgModal = document.querySelector('.bg-gallery-modal');
+  const body = document.querySelector('body');
+  galleryModal.classList.toggle('gallery-plean-modal-show');
+  bgModal.classList.toggle('bg-gallery-modal-show');
+  body.classList.toggle('body-gallery-modal-show');
 
-
-
-
+  const swiper = new Swiper('.mySwiper', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    allowTouchMove: false,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  const swiper2 = new Swiper('.mySwiper2', {
+    spaceBetween: 10,
+    initialSlide: index,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+}
